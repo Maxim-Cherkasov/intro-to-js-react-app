18316f4f720270640c8b591cbbf29b256420ffe7
Maxim-Cherkasov <84859924+Maxim-Cherkasov@users.noreply.github.com>
2022-04-20 09:21:41 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -140,9 +140,115 @@ Usually, the other form is preferred: while(…) {…}.
 
 // Type Conversion
 
+/*
 let input = [5, 9, 6];
 console.log("Type Of [5, 6, 9]: " + typeof(input));
-console.log("Type of [5, 9, 6]");
+console.log("Type of [5, 9, 6]: " + typeof(input.toString()));
+console.log("Type of '2': " + typeof((2)));
+console.log("'True' to Number: " + Number(true));
+console.log("'Hi' to Boolean: " + Boolean('hi'));
+console.log("'NaN' to Number: " + Boolean(NaN));
+console.log("'' to Boolean: " + Boolean(''));
+console.log("'0' to Boolean: " + Boolean('0'));
+*/
+
+// Operators
+
+/*
+Assignment Operators
+
+Assignment operators are used to assign values to operands. “=” operator assigns the
+value of right operand to the left operand, “+=” operator adds the value of right operand
+to left operand and assigns it to left operand, “–=” operator subtracts the value of right
+operand from left operand and assigns it to left operand, “∗=” operator multiplies the
+value of both operands and assigns it to left operand, “/=” operator divides the value
+of left operand to right operand and assigns it to left operand, and lastly, “%=” operand
+calculates the modulus after dividing left operand by right operand and assigns it to left
+operand.
+*/
+
+/*
+(!=) - Inequality. It's a Comparison operator.
+(!) - NOT. A Logical operator that is used to negate 
+the boolean value that is returned. 
+*/
+
+// Ternary Operator
+
+/*
+Ternary operator is made up of three parts: condition, body 1, and body 2. Condition
+and body 1 are separated by “?” operator, whereas both the bodies are separated by “:”
+operator. Body 1 will be executed if the condition is true, whereas body 2 will be executed
+if the condition is false
+*/
+
+var a = 16;
+var b = 17;
+
+console.log('Arithmetic Operators');
+console.log('16 + 2 = ' + (16 + 2));
+console.log('17 % 2 = ' + (17 % 2)); //1
+
+console.log('Comparison Operators');
+console.log('1 == "1" ' + ('1' == 1));
+console.log('1 === "1" ' + ('1' === 1));
+console.log('1 != 2 ' + (1 != 2)); //true
+
+console.log('Assignment Operators');
+console.log('16 += 2 ' + (a += 2));
+console.log(a); //18
+console.log('17 %= 2 ' + (b %= 2));
+console.log(b); //1
+
+console.log('Logical Operators');
+console.log('true || false: ' + (true || false));
+/*
+In classical programming, the logical OR is meant
+to manipulate boolean values only. If any of its 
+arguments are true, it returns true, otherwise it returns false.
+*/
+// Most of the time, OR || is used in an if statement
+// to test if any of the given conditions is true.
+// For example :
+/*
+let hour = 9;
+
+if (hour < 10 || hour > 18) {
+   alert( 'The office is closed.' );
+}
+
+//We can pass more conditions:
+
+let hour = 12;
+let isWeekend = true;
+
+if (hour < 10 || hour > 18 || isWeekend) {
+  alert( 'The office is closed.' ); // it is the weekend
+}
+*/
+console.log(('true && false: ' + (true && false)));
+console.log(typeof(true));
+/*
+In classical programming, AND returns true if
+   both operands are truthy and false otherwise:
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
